d141f8b082b6f40ba8a011354c4f584f1fd6bc79
USER1012007 <rojasbadilloe@gmail.com>
2024-05-21 15:17:17 -0600

    fix afiliations

--- src/components/Afiliations.jsx
+++ src/components/Afiliations.jsx
@@ -6,34 +6,27 @@ import tomza from "../Img/Tomza-logo.png";
 import '../css/Afiliations.css';
 import React, { useEffect, useRef } from 'react';
 
-function Afiliations() {
-
-
-  const affiliationRef = useRef(null);
+const Afiliations = () => {
+  const imageRefs = useRef([]);
 
   useEffect(() => {
-    const affiliationElement = affiliationRef.current;
-
     const handleScroll = () => {
-      if (affiliationElement) {
-        const rect = affiliationElement.getBoundingClientRect();
-        const isVisible =
-          rect.top >= 0 &&
-          rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-          rect.right <= (window.innerWidth || document.documentElement.clientWidth);
-        console.log("rect top", rect.top)
-        console.log("rect left", rect.left)
-        console.log("rect bottom", rect.bottom)
-        console.log("rect right", rect.right)
-        console.log("isVisible", isVisible)
-        console.log()
-        if (isVisible) {
-          affiliationElement.classList.add("active");
-        } else {
-          affiliationElement.classList.remove("active");
+      console.log(imageRefs)
+      imageRefs.current.forEach((imageRef) => {
+        if (imageRef) {
+          const rect = imageRef.getBoundingClientRect();
+          const isVisible =
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+          if (isVisible) {
+            imageRef.classList.add("active");
+          } else {
+            imageRef.classList.remove("active");
+          }
         }
-      }
+      });
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -41,33 +34,36 @@ function Afiliations() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  return (
-    <>
-      <div className="Afiliations">
-        <div ref={affiliationRef}>
-          <img src={natgas} />
-        </div>
-        <div className="Img-x"></div>
-        <div ref={affiliationRef}>
-          <img src={kansas} />
-        </div>
-        <div className="Img-x"></div>
-        <div ref={affiliationRef}>
-          <img src={repsol} />
-        </div>
+  const addToRefs = (el) => {
+    if (el && !imageRefs.current.includes(el)) {
+      imageRefs.current.push(el);
+    }
+  };
 
-        <div className="Img-x"></div>
-        <div ref={affiliationRef}>
-          <img id="GlencoreFilter" src={glencore} />
-        </div>
-        <div className="Img-x"></div>
-        <div ref={affiliationRef}>
-          <img id="GlencoreFilter" src={tomza} className="transparentImgsvg" />
-        </div>
-        <div className="Img-x"></div>
+  return (
+    <div className="Afiliations">
+      <div ref={addToRefs}>
+        <img src={natgas} />
       </div>
-    </>
+      <div className="Img-x"></div>
+      <div ref={addToRefs}>
+        <img src={kansas} />
+      </div>
+      <div className="Img-x"></div>
+      <div ref={addToRefs}>
+        <img src={repsol} />
+      </div>
+      <div className="Img-x"></div>
+      <div ref={addToRefs}>
+        <img id="GlencoreFilter" src={glencore} />
+      </div>
+      <div className="Img-x"></div>
+      <div ref={addToRefs}>
+        <img id="GlencoreFilter" src={tomza} className="transparentImgsvg" />
+      </div>
+      <div className="Img-x"></div>
+    </div>
   );
-}
+};
 
 export default Afiliations;
